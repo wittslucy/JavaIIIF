a3b33ffa1d6b2292eb2070edb6590146f8eb7b24
wittslucy <clarklucy.lc@gmail.com>
2022-03-23 15:35:27 +0000

    Updating the code to read from a URL and parse to a json object

--- JavaJson/src/com/lwitts/javajson/JavaJson.java
+++ JavaJson/src/com/lwitts/javajson/JavaJson.java
@@ -5,118 +5,62 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 
 public class JavaJson {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        //
-//        JSONParser jsonParser = new JSONParser();
-//
-//        try {
-//            JSONObject jsObject = (JSONObject) jsonParser.parse(new FileReader(""));
-//
-//            String value = (String) jsObject.get("Salary");
-//
-//            System.out.println("Salary: " + value);
-//
-//            JSONArray jsonArray = (JSONArray) jsObject.get("contact");
-//            //System.out.println(jsonArray);
-//
-//            Iterator<String> iterator = jsonArray.iterator();
-//            while (iterator.hasNext()){
-//                System.out.println(iterator.next());
-//            }
-//        }
-//        catch (FileNotFoundException e){
-//            e.printStackTrace();
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        catch (ParseException e){
-//            e.printStackTrace();
-//        }
+        URL manifest = new URL("https://iiif.bodleian.ox.ac.uk/iiif/manifest/abd2e479-a15b-4784-bba0-4c8d8d9a19ea.json");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(manifest.openStream()));
 
-        //
-        //boiler-plate for the code
-//        JSONParser jsonParser = new JSONParser();
-//        try {
-//            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(""));
-//
-//
-//
-//            JSONObject js = (JSONObject) jsonObject.get("label");
-//            JSONArray jsonArray = (JSONArray) js.get("en");
-//
-//            System.out.println("Type: " + jsonObject.get("id"));
-//            System.out.println("Label: " + jsonObject.get("label"));
-//            System.out.println(js);
-//            System.out.println(jsonArray);
-//
-//
-//        }
-//        catch (FileNotFoundException e){
-//            e.printStackTrace();
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        catch (ParseException e){
-//            e.printStackTrace();
-//        }
+        String inputLine;
+        String json;
+        StringBuilder sb = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine);
+        in.close();
 
-        //
-        JSONParser jsonParser = new JSONParser();
-        try {
-            //JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(""));
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(""));
+        json = sb.toString();
+        JSONParser jParse = new JSONParser();
+        JSONObject jObj = (JSONObject) jParse.parse(json);
 
-            JSONArray jsonArray = (JSONArray) jsonObject.get("sequences");
 
-            for (Object o : jsonArray) {
-                JSONObject result = (JSONObject) o;
-                JSONArray canvases = (JSONArray) result.get("canvases");
+        JSONArray jsonArray = (JSONArray) jObj.get("sequences");
+        for (Object o : jsonArray) {
+            JSONObject result = (JSONObject) o;
+            JSONArray canvases = (JSONArray) result.get("canvases");
 
-                for (Object cnv : canvases) {
-                    JSONObject canvas = (JSONObject) cnv;
-                    String label = (String) canvas.get("label");
-                    System.out.println(label);
-                    JSONArray images = (JSONArray) canvas.get("images");
+            for (Object cnv : canvases) {
+                JSONObject canvas = (JSONObject) cnv;
+                String label = (String) canvas.get("label");
+                System.out.println(label);
+                JSONArray images = (JSONArray) canvas.get("images");
 
-                    for (Object img : images) {
-                        JSONObject image = (JSONObject) img;
-                        JSONObject resources = (JSONObject) image.get("resource");
+                for (Object img : images) {
+                    JSONObject image = (JSONObject) img;
+                    JSONObject resources = (JSONObject) image.get("resource");
 
-                        String resId = (String) resources.get("@id");
+                    String resId = (String) resources.get("@id");
+                    System.out.println(resId);
+                    Long height = (Long) canvas.get("height");
+                    String heightStr = Long.toString(height);
+                    Long width = (Long) canvas.get("width");
+                    String widthStr = Long.toString(width);
+
+                    //checking to see if it has a .jpg extension, might need more? Or be more generic?
+                    if (resId.contains(".jpg")) {
                         System.out.println(resId);
-                        Long height = (Long) canvas.get("height");
-                        String heightStr = Long.toString(height);
-                        Long width = (Long) canvas.get("width");
-                        String widthStr = Long.toString(width);
-
-                        //checking to see if it has a .jpg extension, might need more? Or be more generic?
-                        if (resId.contains(".jpg")) {
-                            System.out.println(resId);
-                        } else {
-                            String imageUrl = createUrl(resId, heightStr, widthStr);
-                            System.out.println(imageUrl);
-                        }
+                    } else {
+                        String imageUrl = createUrl(resId, heightStr, widthStr);
+                        System.out.println(imageUrl);
                     }
                 }
             }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
+
     }
 
 
